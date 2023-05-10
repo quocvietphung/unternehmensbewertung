e9dc02eed4d4f3778d171371f7045bdb61e3762b
viet <37694614+quocvietphung@users.noreply.github.com>
2023-05-10 19:30:40 +0200

    Install Bootstrap 5.3 alpha 2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 // src/components/Home.js
 import React from 'react';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <header className="Home-header">
                 <h1>Willkommen auf unserer Webseite</h1>
             </header>
+            <Footer />
         </div>
     );
 };
