c3e01fd6dc92457c8594b1c30095fe95a92822d9
viet <37694614+quocvietphung@users.noreply.github.com>
2023-05-12 12:37:26 +0200

    Refactor Unternehmenswertrechner

--- src/components/Unternehmenswertrechner/BasisInfo.js
+++ src/components/Unternehmenswertrechner/BasisInfo.js
@@ -5,17 +5,22 @@ class BasisInfo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            branche: '',
-            lage: '',
+            branche: "",
+            lage: "",
             alter: 0,
-            isValid: false
+            isValid: false,
         };
     }
 
     handleChange = (event, { name, value }) => {
         if (name === "alter" && (value === "+" || value === "-")) {
             const currentValue = this.state.alter;
-            const newValue = value === "+" ? currentValue + 1 : currentValue > 0 ? currentValue - 1 : 0;
+            const newValue =
+                value === "+"
+                    ? currentValue + 1
+                    : currentValue > 0
+                        ? currentValue - 1
+                        : 0;
             this.setState({ alter: newValue }, this.checkValidity);
         } else {
             this.setState({ [name]: value }, this.checkValidity);
@@ -31,26 +36,30 @@ class BasisInfo extends Component {
         }
     };
 
+    handleWeiterClick = () => {
+        this.props.onWeiterClick(); // Gọi hàm xử lý khi người dùng nhấp vào nút "Weiter"
+    };
+
     render() {
-        const { branche, lage, alter, isValid } = this.state;
+        const { branche, lage, alter } = this.state;
 
         const branchOptions = [
-            { key: 'bau', value: '4.8, 0.63', text: 'Bau und Handwerk' },
-            { key: 'beratung', value: '5.0, 0.85', text: 'Beratende Dienstleistung' },
-            { key: 'chemie', value: '5.9, 1.1', text: 'Chemie, Kunststoffe, Papier' },
-            { key: 'elektrotechnik', value: '5.4, 0.84', text: 'Elektrotechnik' },
-            { key: 'fahrzeugbau', value: '5.15, 0.72', text: 'Fahrzeugbau und -zubehör' },
-            { key: 'handel', value: '5.55, 0.73', text: 'Handel und E-Commerce' },
-            { key: 'maschinenbau', value: '5.6, 0.85', text: 'Maschinen- und Anlagenbau' },
-            { key: 'medien', value: '5.3, 1.16', text: 'Medien' },
-            { key: 'nahrungs', value: '5.45, 1.11', text: 'Nahrungs- und Genussmittel' },
-            { key: 'pharma', value: '6.5, 1.64', text: 'Pharma, Bio- und Medizintechnik' },
-            { key: 'software', value: '5.65, 1.56', text: 'Software' },
-            { key: 'telekommunikation', value: '5.65, 1.05', text: 'Telekommunikation' },
-            { key: 'textilien', value: '4.5, 0.81', text: 'Textilien und Bekleidung' },
-            { key: 'transport', value: '4.85, 0.64', text: 'Transport, Logistik und Touristik' },
-            { key: 'umwelttechnik', value: '5.60, 0.85', text:'Umwelttechnik' },
-            { key: 'versorgungswirtschaft', value: '5.60, 0.85', text: 'Versorgungswirtschaft' },
+            { key: "bau", value: "4.8, 0.63", text: "Bau und Handwerk" },
+            { key: "beratung", value: "5.0, 0.85", text: "Beratende Dienstleistung" },
+            { key: "chemie", value: "5.9, 1.1", text: "Chemie, Kunststoffe, Papier" },
+            { key: "elektrotechnik", value: "5.4, 0.84", text: "Elektrotechnik" },
+            { key: "fahrzeugbau", value: "5.15, 0.72", text: "Fahrzeugbau und -zubehör" },
+            { key: "handel", value: "5.55, 0.73", text: "Handel und E-Commerce" },
+            { key: "maschinenbau", value: "5.6, 0.85", text: "Maschinen- und Anlagenbau" },
+            { key: "medien", value: "5.3, 1.16", text: "Medien" },
+            { key: "nahrungs", value: "5.45, 1.11", text: "Nahrungs- und Genussmittel" },
+            { key: "pharma", value: "6.5, 1.64", text: "Pharma, Bio- und Medizintechnik" },
+            { key: "software", value: "5.65, 1.56", text: "Software" },
+            { key: "telekommunikation", value: "5.65, 1.05", text: "Telekommunikation" },
+            { key: "textilien", value: "4.5, 0.81", text: "Textilien und Bekleidung" },
+            { key: "transport", value: "4.85, 0.64", text: "Transport, Logistik und Touristik" },
+            { key: "umwelttechnik", value: "5.60, 0.85", text: "Umwelttechnik" },
+            { key: "versorgungswirtschaft", value: "5.60, 0.85", text: "Versorgungswirtschaft" },
         ];
 
         return (
@@ -78,7 +87,7 @@ class BasisInfo extends Component {
                                         name="lage"
                                         value="städtisch"
                                         label="städtisch"
-                                        checked={lage === 'städtisch'}
+                                        checked={lage === "städtisch"}
                                         onChange={this.handleChange}
                                         required
                                     />
@@ -88,7 +97,7 @@ class BasisInfo extends Component {
                                         name="lage"
                                         value="ländlich"
                                         label="ländlich"
-                                        checked={lage === 'ländlich'}
+                                        checked={lage === "ländlich"}
                                         onChange={this.handleChange}
                                         required
                                     />
@@ -101,7 +110,7 @@ class BasisInfo extends Component {
                                         icon="minus"
                                         data-type="minus"
                                         data-field="alter"
-                                        onClick={(event) => this.handleChange(event, { name: 'alter', value: '-' })}
+                                        onClick={(event) => this.handleChange(event, { name: "alter", value: "-" })}
                                     />
                                     <input
                                         type="number"
@@ -109,21 +118,21 @@ class BasisInfo extends Component {
                                         className="form-control input-number-plusminus"
                                         min="0"
                                         value={alter}
-                                        onChange={(event) => this.handleChange(event, { name: 'alter', value: event.target.value })}
+                                        onChange={(event) => this.handleChange(event, { name: "alter", value: event.target.value })}
                                         required
                                     />
                                     <Button
                                         icon="plus"
                                         data-type="plus"
                                         data-field="alter"
-                                        onClick={(event) => this.handleChange(event, { name: 'alter', value: '+' })}
+                                        onClick={(event) => this.handleChange(event, { name: "alter", value: "+" })}
                                     />
                                 </div>
                             </Form.Field>
                         </Grid.Column>
                     </Grid>
                     <p>* Diese Eingaben sind Pflichtfelder</p>
-                    <Button type="submit" primary disabled={!isValid}>
+                    <Button type="submit" primary onClick={this.handleWeiterClick}>
                         Weiter
                     </Button>
                 </Form>
--- src/components/Unternehmenswertrechner/Unternehmenswertrechner.js
+++ src/components/Unternehmenswertrechner/Unternehmenswertrechner.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import BasisInfo from './BasisInfo';
-import Progress from "./Progress";
+import Progress from './Progress';
 import './Unternehmenswertrechner.scss';
-import Kennzahlen from "./Kennzahlen";
+import Kennzahlen from './Kennzahlen';
 
 class Unternehmenswertrechner extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showKennzahlen: false
+        };
+    }
+
+    handleWeiterClick = () => {
+        this.setState({ showKennzahlen: true });
+    };
+
     render() {
+        const { showKennzahlen } = this.state;
+
         return (
             <div className="Unternehmenswertrechner">
-                <Progress/>
+                <Progress />
                 <div className="unternehmenswertrechner-container">
-                    <BasisInfo/>
-                    <Kennzahlen/>
+                    {showKennzahlen ? (
+                        <Kennzahlen />
+                    ) : (
+                        <BasisInfo onWeiterClick={this.handleWeiterClick} />
+                    )}
                 </div>
             </div>
         );
