46572e4b23229f9719051517ce89170a42ee3686
viet <37694614+quocvietphung@users.noreply.github.com>
2023-05-15 15:32:33 +0200

    Refactor function

--- src/components/Unternehmenswertrechner/BasisInfo.js
+++ src/components/Unternehmenswertrechner/BasisInfo.js
@@ -1,151 +1,146 @@
-import React, {Component} from "react";
+import React, { useState, useEffect } from "react";
 import {Header, Form, Grid, Select, Radio, Button, Divider} from "semantic-ui-react";
 
-class BasisInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            branche: "",
-            lage: "",
-            alter: 0,
-            isValid: false,
-        };
-    }
+const BasisInfo = (props) => {
+    const [branche, setBranche] = useState("");
+    const [lage, setLage] = useState("");
+    const [alter, setAlter] = useState(0);
+    const [isValid, setIsValid] = useState(false);
 
-    handleChange = (event, {name, value}) => {
+    const handleChange = (event, {name, value}) => {
         if (name === "alter" && (value === "+" || value === "-")) {
-            const currentValue = this.state.alter;
+            const currentValue = alter;
             const newValue =
                 value === "+"
                     ? currentValue + 1
                     : currentValue > 0
                         ? currentValue - 1
                         : 0;
-            this.setState({alter: newValue}, this.checkValidity);
+            setAlter(newValue);
         } else {
-            this.setState({[name]: value}, this.checkValidity);
+            if (name === 'branche') setBranche(value);
+            else if (name === 'lage') setLage(value);
         }
     };
 
-    checkValidity = () => {
-        const {branche, lage, alter} = this.state;
+    useEffect(() => {
+        checkValidity();
+    }, [branche, lage, alter]);
+
+    const checkValidity = () => {
         if (branche && lage && alter >= 0) {
-            this.setState({isValid: true});
+            setIsValid(true);
         } else {
-            this.setState({isValid: false});
+            setIsValid(false);
         }
     };
 
-    handleWeiterClick = () => {
-        this.props.onWeiterClick(this.props.sectionName);  // Gọi hàm xử lý khi người dùng nhấp vào nút "Weiter"
+    const handleWeiterClick = () => {
+        props.onWeiterClick(props.sectionName);  // Gọi hàm xử lý khi người dùng nhấp vào nút "Weiter"
     };
 
-    render() {
-        const {branche, lage, alter} = this.state;
+    const branchOptions = [
+        {key: "bau", value: "4.8, 0.63", text: "Bau und Handwerk"},
+        {key: "beratung", value: "5.0, 0.85", text: "Beratende Dienstleistung"},
+        {key: "chemie", value: "5.9, 1.1", text: "Chemie, Kunststoffe, Papier"},
+        {key: "elektrotechnik", value: "5.4, 0.84", text: "Elektrotechnik"},
+        {key: "fahrzeugbau", value: "5.15, 0.72", text: "Fahrzeugbau und -zubehör"},
+        {key: "handel", value: "5.55, 0.73", text: "Handel und E-Commerce"},
+        {key: "maschinenbau", value: "5.6, 0.85", text: "Maschinen- und Anlagenbau"},
+        {key: "medien", value: "5.3, 1.16", text: "Medien"},
+        {key: "nahrungs", value: "5.45, 1.11", text: "Nahrungs- und Genussmittel"},
+        {key: "pharma", value: "6.5, 1.64", text: "Pharma, Bio- und Medizintechnik"},
+        {key: "software", value: "5.65, 1.56", text: "Software"},
+        {key: "telekommunikation", value: "5.65, 1.05", text: "Telekommunikation"},
+        {key: "textilien", value: "4.5, 0.81", text: "Textilien und Bekleidung"},
+        {key: "transport", value: "4.85, 0.64", text: "Transport, Logistik und Touristik"},
+        {key: "umwelttechnik", value: "5.60, 0.85", text: "Umwelttechnik"},
+        {key: "versorgungswirtschaft", value: "5.60, 0.85", text: "Versorgungswirtschaft"},
+    ];
 
-        const branchOptions = [
-            {key: "bau", value: "4.8, 0.63", text: "Bau und Handwerk"},
-            {key: "beratung", value: "5.0, 0.85", text: "Beratende Dienstleistung"},
-            {key: "chemie", value: "5.9, 1.1", text: "Chemie, Kunststoffe, Papier"},
-            {key: "elektrotechnik", value: "5.4, 0.84", text: "Elektrotechnik"},
-            {key: "fahrzeugbau", value: "5.15, 0.72", text: "Fahrzeugbau und -zubehör"},
-            {key: "handel", value: "5.55, 0.73", text: "Handel und E-Commerce"},
-            {key: "maschinenbau", value: "5.6, 0.85", text: "Maschinen- und Anlagenbau"},
-            {key: "medien", value: "5.3, 1.16", text: "Medien"},
-            {key: "nahrungs", value: "5.45, 1.11", text: "Nahrungs- und Genussmittel"},
-            {key: "pharma", value: "6.5, 1.64", text: "Pharma, Bio- und Medizintechnik"},
-            {key: "software", value: "5.65, 1.56", text: "Software"},
-            {key: "telekommunikation", value: "5.65, 1.05", text: "Telekommunikation"},
-            {key: "textilien", value: "4.5, 0.81", text: "Textilien und Bekleidung"},
-            {key: "transport", value: "4.85, 0.64", text: "Transport, Logistik und Touristik"},
-            {key: "umwelttechnik", value: "5.60, 0.85", text: "Umwelttechnik"},
-            {key: "versorgungswirtschaft", value: "5.60, 0.85", text: "Versorgungswirtschaft"},
-        ];
-
-        return (
-            <Grid padded className="shared-section basis-info">
-                <Grid.Column>
-                    <Header as="h2">1. Basisinformationen zum Unternehmen</Header>
-                    <Divider/>
-                    <Form>
-                        <Grid columns={2} stackable>
-                            <Grid.Column>
+    return (
+        <Grid padded className="shared-section basis-info">
+            <Grid.Column>
+                <Header as="h2">1. Basisinformationen zum Unternehmen</Header>
+                <Divider/>
+                <Form>
+                    <Grid columns={2} stackable>
+                        <Grid.Column>
+                            <Form.Field>
+                                <label>Branche*</label>
+                                <Select
+                                    className="wideSelect"
+                                    options={branchOptions}
+                                    name="branche"
+                                    value={branche}
+                                    onChange={handleChange}
+                                    placeholder="Branche auswählen"
+                                    required
+                                />
+                            </Form.Field>
+                            <Form.Group inline>
+                                <label>Lage*</label>
                                 <Form.Field>
-                                    <label>Branche*</label>
-                                    <Select
-                                        className="wideSelect"
-                                        options={branchOptions}
-                                        name="branche"
-                                        value={branche}
-                                        onChange={this.handleChange}
-                                        placeholder="Branche auswählen"
+                                    <Radio
+                                        name="lage"
+                                        value="städtisch"
+                                        label="städtisch"
+                                        checked={lage === "städtisch"}
+                                        onChange={handleChange}
                                         required
                                     />
                                 </Form.Field>
-                                <Form.Group inline>
-                                    <label>Lage*</label>
-                                    <Form.Field>
-                                        <Radio
-                                            name="lage"
-                                            value="städtisch"
-                                            label="städtisch"
-                                            checked={lage === "städtisch"}
-                                            onChange={this.handleChange}
-                                            required
-                                        />
-                                    </Form.Field>
-                                    <Form.Field>
-                                        <Radio
-                                            name="lage"
-                                            value="ländlich"
-                                            label="ländlich"
-                                            checked={lage === "ländlich"}
-                                            onChange={this.handleChange}
-                                            required
-                                        />
-                                    </Form.Field>
-                                </Form.Group>
-                                <Form.Group className="input-group">
-                                    <Form.Field>
-                                        <label>Alter der Firma in Jahren*</label>
-                                    </Form.Field>
-                                </Form.Group>
-                                <Form.Group className="input-container">
-                                    <Button
-                                        icon="minus"
-                                        data-type="minus"
-                                        data-field="alter"
-                                        onClick={(event) => this.handleChange(event, { name: "alter", value: "-" })}
-                                    />
-                                    <input
-                                        type="number"
-                                        name="alter"
-                                        className="form-control input-number-plusminus"
-                                        min="0"
-                                        value={alter}
-                                        onChange={(event) => this.handleChange(event, { name: "alter", value: event.target.value })}
+                                <Form.Field>
+                                    <Radio
+                                        name="lage"
+                                        value="ländlich"
+                                        label="ländlich"
+                                        checked={lage === "ländlich"}
+                                        onChange={handleChange}
                                         required
                                     />
-                                    <Button
-                                        icon="plus"
-                                        data-type="plus"
-                                        data-field="alter"
-                                        onClick={(event) => this.handleChange(event, { name: "alter", value: "+" })}
-                                    />
-                                </Form.Group>
-                            </Grid.Column>
-                        </Grid>
-                        <p>* Diese Eingaben sind Pflichtfelder</p>
-                        <div className="button-container">
-                            <Button type="submit" primary onClick={this.handleWeiterClick}>
-                                Weiter
-                            </Button>
-                        </div>
-                    </Form>
-                </Grid.Column>
-            </Grid>
-        );
-    }
+                                </Form.Field>
+                            </Form.Group>
+                            <Form.Group className="input-group">
+                                <Form.Field>
+                                    <label>Alter der Firma in Jahren*</label>
+                                </Form.Field>
+                            </Form.Group>
+                            <Form.Group className="input-container">
+                                <Button
+                                    icon="minus"
+                                    data-type="minus"
+                                    data-field="alter"
+                                    onClick={(event) => handleChange(event, { name: "alter", value: "-" })}
+                                />
+                                <input
+                                    type="number"
+                                    name="alter"
+                                    className="form-control input-number-plusminus"
+                                    min="0"
+                                    value={alter}
+                                    onChange={(event) => handleChange(event, { name: "alter", value: event.target.value })}
+                                    required
+                                />
+                                <Button
+                                    icon="plus"
+                                    data-type="plus"
+                                    data-field="alter"
+                                    onClick={(event) => handleChange(event, { name: "alter", value: "+" })}
+                                />
+                            </Form.Group>
+                        </Grid.Column>
+                    </Grid>
+                    <p>* Diese Eingaben sind Pflichtfelder</p>
+                    <div className="button-container">
+                        <Button type="submit" primary onClick={handleWeiterClick}>
+                            Weiter
+                        </Button>
+                    </div>
+                </Form>
+            </Grid.Column>
+        </Grid>
+    );
 }
 
 export default BasisInfo;
--- src/components/Unternehmenswertrechner/Unternehmenswertrechner.js
+++ src/components/Unternehmenswertrechner/Unternehmenswertrechner.js
@@ -1,51 +1,35 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import BasisInfo from './BasisInfo';
 import ProgressSection from './ProgressSection';
 import './Unternehmenswertrechner.scss';
 import Kennzahlen from './Kennzahlen';
-import {Header} from "semantic-ui-react";
+import { Header } from 'semantic-ui-react';
 
-class Unternehmenswertrechner extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            activeSection: 'basis',
-            finishedSections: []
-        };
-    }
+const Unternehmenswertrechner = () => {
+    const [activeSection, setActiveSection] = useState('basis');
+    const [finishedSections, setFinishedSections] = useState([]);
 
-    setActiveSection = (section) => {
-        this.setState(state => {
-            const finishedSections = [...state.finishedSections];
-            if (!finishedSections.includes(state.activeSection)) {
-                finishedSections.push(state.activeSection);
-            }
-            return {
-                activeSection: section,
-                finishedSections
-            };
-        });
+    const updateActiveSection = (section) => {
+        if (!finishedSections.includes(activeSection)) {
+            setFinishedSections([...finishedSections, activeSection]);
+        }
+        setActiveSection(section);
     };
 
-    handleWeiterClick = () => {
-        this.setState(prevState => ({
-            activeSection: 'kennzahlen',
-            finishedSections: [...prevState.finishedSections, prevState.activeSection] // Add current activeSection to finished sections
-        }));
+    const handleWeiterClick = () => {
+        setFinishedSections([...finishedSections, activeSection]);
+        setActiveSection('kennzahlen');
     };
 
-    render() {
-        const {activeSection, finishedSections} = this.state;
-        return (
-            <div className="Unternehmenswertrechner">
-                <Header as="h1" className="main-header">Willkommen beim Unternehmenswertrechner</Header>
-                <ProgressSection setActiveSection={this.setActiveSection} activeSection={activeSection} finishedSections={finishedSections}/>
-                <div className="unternehmenswertrechner-container">
-                    {activeSection === 'kennzahlen' ? <Kennzahlen className="shared-section" /> : <BasisInfo sectionName="basis" onWeiterClick={this.handleWeiterClick} className="shared-section" />}
-                </div>
+    return (
+        <div className="Unternehmenswertrechner">
+            <Header as="h1" className="main-header">Willkommen beim Unternehmenswertrechner</Header>
+            <ProgressSection setActiveSection={updateActiveSection} activeSection={activeSection} finishedSections={finishedSections}/>
+            <div className="unternehmenswertrechner-container">
+                {activeSection === 'kennzahlen' ? <Kennzahlen className="shared-section" /> : <BasisInfo sectionName="basis" onWeiterClick={handleWeiterClick} className="shared-section" />}
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Unternehmenswertrechner;
