c61c842797eb6302f16944b6ca1be7e45b079e33
viet <37694614+quocvietphung@users.noreply.github.com>
2023-05-16 18:13:41 +0200

    Refactor SCSS

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {Container} from 'semantic-ui-react';
 import Home from '../Home/Home';
 import Unternehmenswertrechner from '../Unternehmenswertrechner/Unternehmenswertrechner';
 import Footer from './Footer';
@@ -13,15 +13,13 @@ const App = () => {
     return (
         <Router>
             <div className="App">
-                <Navbar />
-                <Container text>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/unternehmenswert-berechnen" element={<Unternehmenswertrechner />} />
-                        <Route path="/test" element={<Kennzahlen />} />
-                    </Routes>
-                </Container>
-                <Footer />
+                <Navbar/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/unternehmenswert-berechnen" element={<Unternehmenswertrechner/>}/>
+                    <Route path="/test" element={<Kennzahlen/>}/>
+                </Routes>
+                <Footer/>
             </div>
         </Router>
     );
