3b49d96eff8ce2866238cc61ba0db295cff7f6e2
viet <37694614+quocvietphung@users.noreply.github.com>
2023-05-16 13:27:40 +0200

    Refactor SCSS

--- src/components/Unternehmenswertrechner/Unternehmenswertrechner.js
+++ src/components/Unternehmenswertrechner/Unternehmenswertrechner.js
@@ -6,27 +6,40 @@ import Kennzahlen from './Kennzahlen';
 import { Header } from 'semantic-ui-react';
 
 const Unternehmenswertrechner = () => {
-    const [activeSection, setActiveSection] = useState('basis');
-    const [finishedSections, setFinishedSections] = useState([]);
+    const [sections, setSections] = useState({
+        activeSection: 'basis',
+        finishedSections: []
+    });
 
     const updateActiveSection = (section) => {
-        if (!finishedSections.includes(activeSection)) {
-            setFinishedSections([...finishedSections, activeSection]);
+        if (!sections.finishedSections.includes(sections.activeSection)) {
+            setSections({
+                ...sections,
+                finishedSections: [...sections.finishedSections, sections.activeSection],
+                activeSection: section
+            });
+        } else {
+            setSections({
+                ...sections,
+                activeSection: section
+            });
         }
-        setActiveSection(section);
     };
 
     const handleWeiterClick = () => {
-        setFinishedSections([...finishedSections, activeSection]);
-        setActiveSection('kennzahlen');
+        setSections({
+            ...sections,
+            finishedSections: [...sections.finishedSections, sections.activeSection],
+            activeSection: 'kennzahlen'
+        });
     };
 
     return (
         <div className="Unternehmenswertrechner">
             <Header as="h1" className="main-header">Willkommen beim Unternehmenswertrechner</Header>
-            <ProgressSection setActiveSection={updateActiveSection} activeSection={activeSection} finishedSections={finishedSections}/>
+            <ProgressSection setActiveSection={updateActiveSection} activeSection={sections.activeSection} finishedSections={sections.finishedSections}/>
             <div className="unternehmenswertrechner-container">
-                {activeSection === 'kennzahlen' ? <Kennzahlen className="shared-section" /> : <BasisInfo sectionName="basis" onWeiterClick={handleWeiterClick} className="shared-section" />}
+                {sections.activeSection === 'kennzahlen' ? <Kennzahlen className="shared-section" /> : <BasisInfo sectionName="basis" onWeiterClick={handleWeiterClick} className="shared-section" />}
             </div>
         </div>
     );
