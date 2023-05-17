9d54b6791974feee2e671c8297f797daaae9d58b
viet <37694614+quocvietphung@users.noreply.github.com>
2023-05-17 16:33:32 +0200

    Refactor Ausgabe

--- src/components/Unternehmenswertrechner/Ausgabe.js
+++ src/components/Unternehmenswertrechner/Ausgabe.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid, Header, Icon, Message, Button } from 'semantic-ui-react';
 
-const Ausgabe = () => (
+const Ausgabe = ({ hasError }) => (
     <Grid className="Ausgabe">
         <Grid.Row className="result-inner-wrapper text-center warning-active">
             <Grid.Column textAlign="center">
@@ -15,36 +15,40 @@ const Ausgabe = () => (
                         <p className="ertragswert">33.4 Mio EUR</p>
                         <Message warning className="warning innacurate-calculation">
                             <p className="my-0">
-                                Das ist ein vorläufig berechneter Wert. Füllen Sie weitere Felder aus, um einen genaueren zu Wert zu
-                                erhalten.
+                                Das ist ein vorläufig berechneter Wert. Füllen Sie weitere Felder aus, um einen genaueren zu Wert zu erhalten.
                             </p>
                         </Message>
                     </Grid.Column>
-                    <Grid.Column className="input-error">
-                        <p>
-                            <strong>Wert ungültig</strong>
-                        </p>
-                        <p>Ertragswert kann nicht berechnet werden</p>
-                    </Grid.Column>
+                    {hasError && (
+                        <Grid.Column className="input-error">
+                            <p>
+                                <strong>Wert ungültig</strong>
+                            </p>
+                            <p>Ertragswert kann nicht berechnet werden</p>
+                        </Grid.Column>
+                    )}
                 </Grid>
-                <Message negative className="error-message negative-entry">
-                    <p>
-                        Bitte beachten Sie, dass unser Unternehmenswertrechner keine negativen Werte für dieses Feld erlaubt. Für eine
-                        individuelle Berechnung können Sie gern direkt Kontakt mit uns aufnehmen.
-                    </p>
-                    <Button icon href="mailto:info@everto-consulting.de" title="Jetzt Kontakt aufnehmen">
-                        <Icon name="mail" />
-                    </Button>
-                </Message>
-                <Message className="error-message small-entry">
-                    <p>
-                        Die von Ihnen eingegeben Kennzahlen sind leider zu gering, um einen Unternehmenswert zu berechnen. Für eine
-                        individuelle Berechnung können Sie gern direkt Kontakt mit uns aufnehmen.
-                    </p>
-                    <Button icon href="mailto:info@everto-consulting.de" title="Jetzt Kontakt aufnehmen">
-                        <Icon name="mail" />
-                    </Button>
-                </Message>
+                {hasError ? (
+                    <Message negative className="error-message negative-entry">
+                        <p>
+                            Bitte beachten Sie, dass unser Unternehmenswertrechner keine negativen Werte für dieses Feld erlaubt. Für eine individuelle
+                            Berechnung können Sie gern direkt Kontakt mit uns aufnehmen.
+                        </p>
+                        <Button icon href="mailto:info@everto-consulting.de" title="Jetzt Kontakt aufnehmen">
+                            <Icon name="mail" />
+                        </Button>
+                    </Message>
+                ) : (
+                    <Message className="error-message small-entry">
+                        <p>
+                            Die von Ihnen eingegeben Kennzahlen sind leider zu gering, um einen Unternehmenswert zu berechnen. Für eine individuelle
+                            Berechnung können Sie gern direkt Kontakt mit uns aufnehmen.
+                        </p>
+                        <Button icon href="mailto:info@everto-consulting.de" title="Jetzt Kontakt aufnehmen">
+                            <Icon name="mail" />
+                        </Button>
+                    </Message>
+                )}
             </Grid.Column>
         </Grid.Row>
     </Grid>
