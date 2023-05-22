1905f0b37df775e6b34e912b974f5f49406d23ba
viet <37694614+quocvietphung@users.noreply.github.com>
2023-05-22 12:12:53 +0200

    Refactor Kennzahlen

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,7 @@ import Home from '../Home/Home';
 import Unternehmenswertrechner from '../Unternehmenswertrechner/Unternehmenswertrechner';
 import Footer from './Footer';
 import Navbar from './Navbar';
-import Kennzahlen from "../Unternehmenswertrechner/Kennzahlen";
+import Test from "../Unternehmenswertrechner/Test";
 import './App.scss';
 
 const App = () => {
@@ -16,7 +16,7 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path="/unternehmenswert-berechnen" element={<Unternehmenswertrechner/>}/>
-                    <Route path="/test" element={<Kennzahlen/>}/>
+                    <Route path="/test" element={<Test/>}/>
                 </Routes>
                 <Footer/>
             </div>
