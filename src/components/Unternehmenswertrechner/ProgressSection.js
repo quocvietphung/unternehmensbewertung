2320227507ecb9898786d0febdf097ba1027f4a2
viet <37694614+quocvietphung@users.noreply.github.com>
2023-05-23 12:52:15 +0200

    Refactor

--- src/components/Unternehmenswertrechner/ProgressSection.js
+++ src/components/Unternehmenswertrechner/ProgressSection.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
 
+const sections = ['basis', 'kennzahlen', 'bereinigung', 'equity', 'qualitaet', 'anlass'];
+
 const ProgressSection = ({ setActiveSection, activeSection, finishedSections }) => {
     const handleItemClick = (target) => {
         if (activeSection === target || finishedSections.includes(target)) {
@@ -12,54 +14,17 @@ const ProgressSection = ({ setActiveSection, activeSection, finishedSections }) 
         <div className="nav-wrapper">
             <Grid columns={6}>
                 <Grid.Row>
-                    <Grid.Column
-                        className={`nav-item ${finishedSections.includes('basis') ? 'finished' : activeSection === 'basis' ? 'active' : 'inactive'}`}
-                        data-target="basis"
-                        onClick={() => handleItemClick('basis')}
-                    >
-                        <div className="nav-border"></div>
-                        <div className="heading">Basis-Infos</div>
-                    </Grid.Column>
-                    <Grid.Column
-                        className={`nav-item ${finishedSections.includes('kennzahlen') ? 'finished' : activeSection === 'kennzahlen' ? 'active' : 'inactive'}`}
-                        data-target="kennzahlen"
-                        onClick={() => handleItemClick('kennzahlen')}
-                    >
-                        <div className="nav-border"></div>
-                        <div className="heading">Kennzahlen</div>
-                    </Grid.Column>
-                    <Grid.Column
-                        className={`nav-item ${activeSection === 'bereinigung' ? 'inactive' : 'inactive'}`}
-                        data-target="bereinigung"
-                        onClick={() => handleItemClick('bereinigung')}
-                    >
-                        <div className="nav-border"></div>
-                        <div className="heading">Bereinigung</div>
-                    </Grid.Column>
-                    <Grid.Column
-                        className={`nav-item ${activeSection === 'equity' ? 'inactive' : 'inactive'}`}
-                        data-target="equity"
-                        onClick={() => handleItemClick('equity')}
-                    >
-                        <div className="nav-border"></div>
-                        <div className="heading">Equity Bridge</div>
-                    </Grid.Column>
-                    <Grid.Column
-                        className={`nav-item ${activeSection === 'qualitaet' ? 'inactive' : 'inactive'}`}
-                        data-target="qualitaet"
-                        onClick={() => handleItemClick('qualitaet')}
-                    >
-                        <div className="nav-border"></div>
-                        <div className="heading">Qualität</div>
-                    </Grid.Column>
-                    <Grid.Column
-                        className={`nav-item ${activeSection === 'anlass' ? 'inactive' : 'inactive'}`}
-                        data-target="anlass"
-                        onClick={() => handleItemClick('anlass')}
-                    >
-                        <div className="nav-border"></div>
-                        <div className="heading">Anlass</div>
-                    </Grid.Column>
+                    {sections.map(section => (
+                        <Grid.Column
+                            key={section}
+                            className={`nav-item ${finishedSections.includes(section) ? 'finished' : activeSection === section ? 'active' : 'inactive'}`}
+                            data-target={section}
+                            onClick={() => handleItemClick(section)}
+                        >
+                            <div className="nav-border"></div>
+                            <div className="heading">{section}</div>
+                        </Grid.Column>
+                    ))}
                 </Grid.Row>
             </Grid>
         </div>
