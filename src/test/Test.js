0e6a3c121ad088df73990942a7a30360b8350f54
viet <37694614+quocvietphung@users.noreply.github.com>
2023-05-24 15:15:10 +0200

    Refactor

--- src/test/Test.js
+++ src/test/Test.js
@@ -1,205 +1,57 @@
 import React, { useState } from 'react';
-import { Grid, Header, Form, Input, Popup, Divider, Icon, Segment, Label } from 'semantic-ui-react';
+import { Grid, Header, Divider, Form, Input, Label } from 'semantic-ui-react';
 import './Test.scss';
 
 const Test = () => {
-    const [popoverData, setPopoverData] = useState({
-        showPopover1: false,
-        showPopover2: false,
-    });
+    const [bargeldBestand, setBargeldBestand] = useState('');
+    const [finanzSchulden, setFinanzSchulden] = useState('');
 
-    const handlePopupOpen = (popover) => {
-        setPopoverData((prevState) => ({ ...prevState, [popover]: true }));
+    const handleBargeldBestandChange = (e) => {
+        setBargeldBestand(e.target.value);
     };
 
-    const handlePopupClose = (popover) => {
-        setPopoverData((prevState) => ({ ...prevState, [popover]: false }));
-    };
-
-    const handleInputChange = (e) => {
-        // Handle logic when the input changes here
-    };
-
-    const renderPopupTrigger = (popover) => (
-        <span className="question-mark-icon" onClick={() => handlePopupOpen(popover)}>
-            <Icon name="question circle" />
-        </span>
-    );
-
-    const renderFormFields = () => {
-        const years = ['2020', '2021', '2022', '2023 (Prognose)'];
-        return (
-            <Grid>
-                {years.map((year, index) => (
-                    <Grid.Column key={index} width={4}>
-                        <Form.Field>
-                            <label htmlFor={`gehalt[${index}]`} className="form-label">
-                                {year}
-                            </label>
-                            <Input
-                                type="text"
-                                className="form-text input-number ebit-clean-calc"
-                                name={`gehalt[${index}]`}
-                                required
-                                pattern="\d*"
-                                data-gtm-form-interact-field-id={index + 12}
-                                onChange={handleInputChange}
-                            />
-                            <div className="invalid-feedback negative-number" style={{ display: 'none' }}>
-                                Keine negativen Eingaben erlaubt.
-                            </div>
-                            <div className="invalid-feedback">Das ist ein Pflichtfeld</div>
-                        </Form.Field>
-                    </Grid.Column>
-                ))}
-            </Grid>
-        );
-    };
-
-    const renderAdditionalAdjustments = () => {
-        const years = ['2020', '2021', '2022', '2023 (Prognose)'];
-        return (
-            <Grid>
-                {years.map((year, index) => (
-                    <Grid.Column key={index} width={4}>
-                        <Form.Field>
-                            <label htmlFor={`anpassungEbit[${index}]`} className="form-label">
-                                Anpassung {year}
-                            </label>
-                            <Input
-                                type="text"
-                                className="form-text input-number not-required negative ebit-clean-calc"
-                                name={`anpassungEbit[${index}]`}
-                                onChange={handleInputChange}
-                            />
-                        </Form.Field>
-                        <Form.Field>
-                            <label className="form-label">Bereinigtes EBIT</label>
-                            <Input
-                                type="text"
-                                className="form-text input-number not-required disabled-all"
-                                name={`bereinigungEbit[${index}]`}
-                                onChange={handleInputChange}
-                            />
-                            <div className="invalid-feedback negative-number" style={{ display: 'none' }}>
-                                Keine negativen Eingaben erlaubt.
-                            </div>
-                        </Form.Field>
-                    </Grid.Column>
-                ))}
-                <Form.Field>
-                    <label className="form-label" htmlFor="erklaerungAnpassungEbit">
-                        Fassen Sie die oben gemachten Anpassungen kurz in eigenen Worten zusammen. Dies erscheint als Notiz auf dem Firmenwert-Report.
-                    </label>
-                    <Input className="form-text erklaerungAnpassungEbit" type="text" name="erklaerungAnpassungEbit" />
-                </Form.Field>
-            </Grid>
-        );
+    const handleFinanzSchuldenChange = (e) => {
+        setFinanzSchulden(e.target.value);
     };
 
     return (
-        <Grid padded className="Test">
-            <Grid.Column>
-                <Header as="h2">3. Bereinigung des EBITs</Header>
-                <Divider />
-                <Segment>
-                    <Form>
-                        <Form.Field>
-                            <h3>
-                                Ausgezahlte Vergütung der Geschäftsführung in EUR
-                                <span className="required-mark">*</span>
-                                <Popup
-                                    content="Als Bruttolohn inklusive allen Arbeitgeberkosten"
-                                    position="top center"
-                                    trigger={renderPopupTrigger('showPopover1')}
-                                    open={popoverData.showPopover1}
-                                    onOpen={() => handlePopupOpen('showPopover1')}
-                                    onClose={() => handlePopupClose('showPopover1')}
-                                />
-                            </h3>
-                            {renderFormFields()}
-                        </Form.Field>
+        <Grid padded className="test-component">
+            <Grid.Column className="test-column">
+                <Header as="h2" className="equity-bridge-header">4. Equity Bridge</Header>
+                <Divider/>
 
-                        <Divider />
+                <Header as="h3" className="liquide-mittel-header">
+                    Liquide Mittel &amp; Verbindlichkeiten
+                    <Label color="red" horizontal className="required-label">*</Label>
+                </Header>
 
-                        <Form.Field>
-                            <h3>
-                                Branchenübliche Vergütung einer angestellten Geschäftsführung in EUR
-                                <span className="required-mark">*</span>
-                                <Popup
-                                    content="Falls man einen (neuen) externen Geschäftsführer zu Marktbedingungen einstellen müsste, wie hoch wären dafür die gesamten Personalkosten pro Jahr etwa (Bruttolohn inklusive allen Arbeitgeberkosten)?"
-                                    position="top center"
-                                    trigger={renderPopupTrigger('showPopover2')}
-                                    open={popoverData.showPopover2}
-                                    onOpen={() => handlePopupOpen('showPopover2')}
-                                    onClose={() => handlePopupClose('showPopover2')}
-                                />
-                            </h3>
-                            <label htmlFor="typischGehalt" className="form-label">
-                                Branchenübliche Vergütung
-                            </label>
-                            <Input
-                                type="text"
-                                className="form-text input-number ebit-clean-calc"
-                                name="typischGehalt"
-                                required
-                                pattern="\d*"
-                                data-gtm-form-interact-field-id={16}
-                                onChange={handleInputChange}
-                            />
-                            <div className="invalid-feedback negative-number" style={{ display: 'none' }}>
-                                Keine negativen Eingaben erlaubt.
-                            </div>
-                            <div className="invalid-feedback">Das ist ein Pflichtfeld</div>
-                        </Form.Field>
+                <Form.Field className="form-field">
+                    <label className="form-label">Aktueller Bargeldbestand (Bankkonten und andere bargeldähnliche Guthaben)</label>
+                    <Input
+                        type="number"
+                        name="bargeldBestand"
+                        value={bargeldBestand}
+                        onChange={handleBargeldBestandChange}
+                        required
+                        className="form-input"
+                    />
+                    {bargeldBestand < 0 && <p className="error-message">Keine negativen Eingaben erlaubt.</p>}
+                    {bargeldBestand === '' && <p className="error-message">Das ist ein Pflichtfeld</p>}
+                </Form.Field>
 
-                        <Divider />
-
-                        <Form.Field>
-                            <h3>
-                                Sonstige Bereinigungen des EBITs<span className="required-mark">*</span>
-                            </h3>
-                            <p>
-                                Kleine und mittelständische Unternehmen weisen in der Regel einen zu niedrigen Gewinn aus. Dies liegt einerseits an der Bestrebung des
-                                Unternehmers seine Steuerlast zu reduzieren andererseits, da bei inhabergeführten Betrieben teilweise private und betriebliche Ebenen
-                                vermischt werden. Um die wirkliche Ertragskraft des Unternehmens für einen Käufer aufzuzeigen und ein repräsentatives Ergebnis in der
-                                Unternehmensbewertung zu erhalten, muss der EBIT bereinigt werden. Ziel ist es die tatsächliche Ertragskraft des Unternehmens zu
-                                ermitteln.
-                            </p>
-                            <p>
-                                Folgend sind mögliche Gründe für eine Bereinigung des EBITS aufgelistet.{' '}
-                                <strong>Das Bereinigen des EBITS ist für ein erstes Ergebnis nicht notwendig und kann übersprungen werden</strong>. Das sorgfältige
-                                Bereinigen würde jedoch den Unternehmenswert repräsentativer machen.
-                            </p>
-                            <ul>
-                                <li>
-                                    Hinzurechnen von nicht betriebsnotwendigen Aufwendungen, z.B. private Ausgaben wie Reisespesen, Kfz-Kosten, Handyrechnungen, Löhne
-                                    für Angestellte und Familienmitglieder etc.
-                                </li>
-                                <li>Außerordentliche Abschreibungen</li>
-                                <li>Außerordentliche Erträge und / oder außerordentliche Aufwendungen</li>
-                                <li>Bildung von außerordentlichen Rückstellungen oder Auflösung derselben</li>
-                                <li>Mieten und Pachten an Gesellschafter oder nahestehende Personen die nicht marktüblich sind.</li>
-                                <li>
-                                    Gehälter von Gesellschaftern oder diesen nahen stehenden Personen, einschließlich Nebenleistungen u. ä. die oberhalb des marktüblichen
-                                    Niveaus sind und quasi teilweise Gewinnvorwegentnahmecharakter haben.
-                                </li>
-                                <li>
-                                    Gehälter einschließlich Nebenleistungen von Arbeitnehmern, die in enger persönlicher oder verwandtschaftlicher Beziehung zu Gesellschaftern
-                                    stehen oder die selbst Gesellschafter sind, die oberhalb des marktüblichen Niveaus sind und quasi teilweise Gewinnvorwegentnahmecharakter
-                                    haben.
-                                </li>
-                            </ul>
-
-                            <p> Bitte geben Sie hier sonstige außerplanmäßige Einnahmen oder Ausgaben ("-") in EUR an, um den EBIT zu bereinigen.</p>
-
-                            {renderAdditionalAdjustments()}
-                        </Form.Field>
-                        <Form.Field>
-                            <p>* Diese Eingaben sind Pflichtfelder</p>
-                        </Form.Field>
-                    </Form>
-                </Segment>
+                <Form.Field className="form-field">
+                    <label className="form-label">Aktuelle Finanzschulden (ohne Hypotheken)</label>
+                    <Input
+                        type="number"
+                        name="finanzSchulden"
+                        value={finanzSchulden}
+                        onChange={handleFinanzSchuldenChange}
+                        required
+                        className="form-input"
+                    />
+                    {finanzSchulden < 0 && <p className="error-message">Keine negativen Eingaben erlaubt.</p>}
+                    {finanzSchulden === '' && <p className="error-message">Das ist ein Pflichtfeld</p>}
+                </Form.Field>
             </Grid.Column>
         </Grid>
     );
