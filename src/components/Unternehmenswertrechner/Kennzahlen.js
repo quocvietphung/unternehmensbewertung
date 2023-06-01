78c4d2746d41a341bd5aa80291065016aff28400
viet <37694614+quocvietphung@users.noreply.github.com>
2023-06-01 12:43:56 +0200

    Refactor

--- src/components/Unternehmenswertrechner/Kennzahlen.js
+++ src/components/Unternehmenswertrechner/Kennzahlen.js
@@ -7,9 +7,32 @@ const Kennzahlen = (props) => {
         years: ["2020", "2021", "2022"].concat(checked ? ["2023"] : []),
         options: ['ganz untypisch', 'eher untypisch', 'nur teilweise typisch', 'eher typisch', 'typisch']
     };
+    const [umsatz, setUmsatz] = useState(Array(kennzahlen.years.length).fill(25000000));
+    const [ebit, setEbit] = useState(Array(kennzahlen.years.length).fill(5000000));
+    const [selectedGewinnOptions, setSelectedGewinnOptions] = useState(Array(kennzahlen.years.length).fill(''));
 
     const handleCheckboxChange = () => {
         setChecked(!checked);
+    };
+
+    const handleChange = (index, type, value) => {
+        if (type === 'umsatz') {
+            const newUmsatz = [...umsatz];
+            newUmsatz[index] = value;
+            setUmsatz(newUmsatz);
+        } else if (type === 'ebit') {
+            const newEbit = [...ebit];
+            newEbit[index] = value;
+            setEbit(newEbit);
+        } else if (type === 'selectedGewinnTypischOptions') {
+            const newSelectedGewinnOptions = [...selectedGewinnOptions];
+            newSelectedGewinnOptions[index] = value;
+            setSelectedGewinnOptions(newSelectedGewinnOptions);
+        }
+    };
+
+    const handleWeiterClick = () => {
+        props.onWeiterClick();
     };
 
     return (
@@ -34,7 +57,7 @@ const Kennzahlen = (props) => {
                     </Label>
                     <Segment>
                         <Form>
-                            {years.map((year, index) => (
+                            {kennzahlen.years.map((year, index) => (
                                 <Form.Group className="form-group" key={year}>
                                     <Form.Field width={3} className="form-label">
                                         <label>Umsatz {year}</label>
@@ -45,14 +68,14 @@ const Kennzahlen = (props) => {
                                             min="100000"
                                             max="50000000"
                                             step="50000"
-                                            value={kennzahlen.umsatz[index]}
+                                            value={umsatz[index]}
                                             onChange={(e) => handleChange(index, 'umsatz', e.target.value)}
                                         />
                                     </Form.Field>
                                     <Form.Field width={3} className="form-input">
                                         <input
                                             type="text"
-                                            value={kennzahlen.umsatz[index]}
+                                            value={umsatz[index]}
                                             readOnly
                                         />
                                     </Form.Field>
@@ -68,7 +91,7 @@ const Kennzahlen = (props) => {
                     </Label>
                     <Segment>
                         <Form>
-                            {years.map((year, index) => (
+                            {kennzahlen.years.map((year, index) => (
                                 <Form.Group className="form-group" key={year}>
                                     <Form.Field width={3} className="form-label">
                                         <label>Ebit {year}</label>
@@ -79,14 +102,14 @@ const Kennzahlen = (props) => {
                                             min="0"
                                             max="10000000"
                                             step="1000"
-                                            value={kennzahlen.ebit[index]}
+                                            value={ebit[index]}
                                             onChange={(e) => handleChange(index, 'ebit', e.target.value)}
                                         />
                                     </Form.Field>
                                     <Form.Field width={3} className="form-input">
                                         <input
                                             type="text"
-                                            value={kennzahlen.ebit[index]}
+                                            value={ebit[index]}
                                             readOnly
                                         />
                                     </Form.Field>
@@ -101,21 +124,21 @@ const Kennzahlen = (props) => {
                     <Segment>
                         <Segment.Group horizontal className="segment-group">
                             <Segment></Segment>
-                            {kennzahlen.gewinnTypisch.map((option, index) => (
+                            {kennzahlen.options.map((option, index) => (
                                 <Segment key={index} textAlign="center">{option}</Segment>
                             ))}
                         </Segment.Group>
-                        {kennzahlen.year.map((label, index) => (
+                        {kennzahlen.years.map((label, index) => (
                             <Segment.Group horizontal className="segment-group" key={label}>
                                 <Segment>Gewinn {label}</Segment>
-                                {kennzahlen.gewinnTypisch.map((option, i) => (
+                                {kennzahlen.options.map((option, i) => (
                                     <Segment textAlign="center" key={i}>
                                         <Form.Field>
                                             <Radio
                                                 className="form-check-input"
                                                 name={`gewinnYears[${index}]`}
                                                 value={option}
-                                                checked={selectedGewinnTypischOptions[index]?.value === option}
+                                                checked={selectedGewinnOptions[index] === option}
                                                 onChange={() => handleChange(index, 'selectedGewinnTypischOptions', option)}
                                                 required
                                             />
@@ -133,7 +156,7 @@ const Kennzahlen = (props) => {
                     <Form.Field>
                         <div className="button-container">
                             <Button className="click-back" onClick={props.onZuruckClick}>Zurück</Button>
-                            <Button className="click-continue" primary type="submit" onClick={handleWeiterClick}>
+                            <Button className="click-continue" primary type="submit" onClick={props.handleWeiterClick}>
                                 Weiter
                             </Button>
                         </div>
