e6724cffa9f6326ec2a590eeccfaea3dde7475b9
viet <37694614+quocvietphung@users.noreply.github.com>
2023-06-01 16:13:25 +0200

    Refactor

--- src/components/Unternehmenswertrechner/Kennzahlen.js
+++ src/components/Unternehmenswertrechner/Kennzahlen.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { Checkbox, Label, Grid, Header, Segment, Form, Divider, Button, Radio } from 'semantic-ui-react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setUmsatz, setEbit, setGewinnTypisch, setChecked } from '../../redux/kennzahlenSlice';
@@ -6,16 +6,23 @@ import { setUmsatz, setEbit, setGewinnTypisch, setChecked } from '../../redux/ke
 const Kennzahlen = (props) => {
     const dispatch = useDispatch();
     const checked = useSelector((state) => state.kennzahlen.checked);
-    const umsatz = useSelector((state) => state.kennzahlen.umsatz);
-    const ebit = useSelector((state) => state.kennzahlen.ebit);
-    const gewinnTypisch = useSelector((state) => state.kennzahlen.gewinnTypisch);
+    const umsatz = useSelector((state) => state.kennzahlen.kennzahlen.umsatz);
+    const ebit = useSelector((state) => state.kennzahlen.kennzahlen.ebit);
+    const gewinnTypisch = useSelector((state) => state.kennzahlen.kennzahlen.gewinnTypisch);
 
     const kennzahlen = {
-        umsatzYears: ["Umsatz 2020", "Umsatz 2021", "Umsatz 2022"].concat(checked ? ["Pronose 2023"] : []),
-        ebitYears: ["Ebit 2020", "Ebit 2021", "Ebit 2022"].concat(checked ? ["Pronose 2023"] : []),
-        gewinnYears: [" Gewinn 2020", "Gewinn 2021", "Gewinn 2022"].concat(checked ? ["Pronose 2023"] : []),
+        umsatzYears: ["Umsatz 2020", "Umsatz 2021", "Umsatz 2022"].concat(checked ? ["Prognose 2023"] : []),
+        ebitYears: ["Ebit 2020", "Ebit 2021", "Ebit 2022"].concat(checked ? ["Prognose 2023"] : []),
+        gewinnYears: ["Gewinn 2020", "Gewinn 2021", "Gewinn 2022"].concat(checked ? ["Prognose 2023"] : []),
         options: ['ganz untypisch', 'eher untypisch', 'nur teilweise typisch', 'eher typisch', 'typisch']
     };
+
+    useEffect(() => {
+        console.log("checked:", checked);
+        console.log("umsatz:", umsatz);
+        console.log("ebit:", ebit);
+        console.log("gewinnTypisch:", gewinnTypisch);
+    }, [checked, umsatz, ebit, gewinnTypisch]);
 
     const handleCheckboxChange = () => {
         dispatch(setChecked(!checked));
@@ -23,16 +30,20 @@ const Kennzahlen = (props) => {
 
     const handleChange = (index, type, value) => {
         if (type === 'umsatz') {
-            const newUmsatz = [...umsatz];
+            const newUmsatz = [...(umsatz || [])];
             newUmsatz[index] = value;
             dispatch(setUmsatz(newUmsatz));
         } else if (type === 'ebit') {
-            const newEbit = [...ebit];
+            const newEbit = [...(ebit || [])];
             newEbit[index] = value;
             dispatch(setEbit(newEbit));
         } else if (type === 'gewinnTypisch') {
-            const newGewinnTypisch = [...gewinnTypisch];
-            newGewinnTypisch[index] = value;
+            const newGewinnTypisch = {
+                ...gewinnTypisch,
+                gewinn: gewinnTypisch.gewinn.map((item, i) =>
+                    i === index ? { ...item, value } : item
+                )
+            };
             dispatch(setGewinnTypisch(newGewinnTypisch));
         }
     };
@@ -74,14 +85,14 @@ const Kennzahlen = (props) => {
                                             min="100000"
                                             max="50000000"
                                             step="50000"
-                                            value={umsatz[index]}
+                                            value={(umsatz && umsatz[index]?.value) || ""}
                                             onChange={(e) => handleChange(index, 'umsatz', e.target.value)}
                                         />
                                     </Form.Field>
                                     <Form.Field width={3} className="form-input">
                                         <input
                                             type="text"
-                                            value={umsatz[index]}
+                                            value={(umsatz && umsatz[index]?.value) || ""}
                                             readOnly
                                         />
                                     </Form.Field>
@@ -108,14 +119,14 @@ const Kennzahlen = (props) => {
                                             min="0"
                                             max="10000000"
                                             step="1000"
-                                            value={ebit[index]}
+                                            value={(ebit && ebit[index]?.value) || ""}
                                             onChange={(e) => handleChange(index, 'ebit', e.target.value)}
                                         />
                                     </Form.Field>
                                     <Form.Field width={3} className="form-input">
                                         <input
                                             type="text"
-                                            value={ebit[index]}
+                                            value={(ebit && ebit[index]?.value) || ""}
                                             readOnly
                                         />
                                     </Form.Field>
@@ -130,21 +141,21 @@ const Kennzahlen = (props) => {
                     <Segment>
                         <Segment.Group horizontal className="segment-group">
                             <Segment></Segment>
-                            {kennzahlen.options.map((option, index) => (
+                            {gewinnTypisch.options.map((option, index) => (
                                 <Segment key={index} textAlign="center">{option}</Segment>
                             ))}
                         </Segment.Group>
-                        {kennzahlen.gewinnYears.map((label, index) => (
-                            <Segment.Group horizontal className="segment-group" key={label}>
-                                <Segment>{label}</Segment>
-                                {kennzahlen.options.map((option, i) => (
+                        {gewinnTypisch.gewinn.map((item, index) => (
+                            <Segment.Group horizontal className="segment-group" key={item.title}>
+                                <Segment>{item.title}</Segment>
+                                {gewinnTypisch.options.map((option, i) => (
                                     <Segment textAlign="center" key={i}>
                                         <Form.Field>
                                             <Radio
                                                 className="form-check-input"
                                                 name={`gewinnYears[${index}]`}
                                                 value={option}
-                                                checked={gewinnTypisch[index] === option}
+                                                checked={gewinnTypisch.gewinn[index]?.value === option}
                                                 onChange={() => handleChange(index, 'gewinnTypisch', option)}
                                                 required
                                             />
--- src/redux/kennzahlenSlice.js
+++ src/redux/kennzahlenSlice.js
@@ -2,9 +2,68 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     checked: false,
-    umsatz: {},
-    ebit: {},
-    gewinnTypisch: {},
+    kennzahlen: {
+        umsatz: [
+            {
+                title: "Umsatz 2020",
+                year: 2020,
+                value: 25000000,
+            },
+            {
+                title: "Umsatz 2021",
+                year: 2021,
+                value: 25000000,
+            },
+            {
+                title: "Umsatz 2022",
+                year: 2022,
+                value: 25000000,
+            }
+        ],
+        ebit: [
+            {
+                title: "Ebit 2020",
+                year: 2020,
+                value: 5000000
+            },
+            {
+                title: "Ebit 2021",
+                year: 2021,
+                value: 5000000
+            },
+            {
+                title: "Ebit 2022",
+                year: 2022,
+                value: 5000000
+            }
+        ],
+        gewinnTypisch: {
+            gewinn: [
+                {
+                    title: "Gewinn 2020",
+                    year: 2020,
+                    value: ""
+                },
+                {
+                    title: "Gewinn 2021",
+                    year: 2021,
+                    value: ""
+                },
+                {
+                    title: "Gewinn 2022",
+                    year: 2022,
+                    value: ""
+                }
+            ],
+            options: [
+                'ganz untypisch',
+                'eher untypisch',
+                'nur teilweise typisch',
+                'eher typisch',
+                'typisch'
+            ]
+        }
+    }
 };
 
 const kennzahlenSlice = createSlice({
@@ -15,16 +74,22 @@ const kennzahlenSlice = createSlice({
             state.checked = action.payload;
         },
         setUmsatz: (state, action) => {
-            state.umsatz = action.payload;
+            state.kennzahlen.umsatz = action.payload;
         },
         setEbit: (state, action) => {
-            state.ebit = action.payload;
+            state.kennzahlen.ebit = action.payload;
         },
         setGewinnTypisch: (state, action) => {
-            state.gewinnTypisch = action.payload;
-        },
-    },
+            state.kennzahlen.gewinnTypisch = action.payload;
+        }
+    }
 });
 
-export const { setChecked, setUmsatz, setEbit, setGewinnTypisch } = kennzahlenSlice.actions;
+export const {
+    setChecked,
+    setUmsatz,
+    setEbit,
+    setGewinnTypisch
+} = kennzahlenSlice.actions;
+
 export default kennzahlenSlice.reducer;
