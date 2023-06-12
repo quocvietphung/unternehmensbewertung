d61250700dd8e77971c3413c58e79ada386baaef
viet <37694614+quocvietphung@users.noreply.github.com>
2023-06-12 14:33:07 +0200

    Refactor

--- src/components/Unternehmenswertrechner/Bereinigung.js
+++ src/components/Unternehmenswertrechner/Bereinigung.js
@@ -61,25 +61,33 @@ const Bereinigung = (props) => {
 
     const calculateBereinigungEbit = () => {
         const { gehalt, anpassungEbit, typischGehalt } = bereinigungData;
+        const updateBereinigungEbit = (bereinigungEbits) => {
+            return bereinigungEbits.map((item, index) => {
+                const kennzahlenDataEbit = parseFloat(bereinigungEbits[index].value) || 0;
+                const gehaltValue = parseFloat(gehalt[index].value) || 0;
+                const anpassungEbitValue = parseFloat(anpassungEbit[index].value) || 0;
+                const typischGehaltValue = parseFloat(typischGehalt) || 0;
+                const bereinigtesEbitValue = (kennzahlenDataEbit + gehaltValue + anpassungEbitValue) - typischGehaltValue;
 
-        const updatedBereinigungEbit = bereinigungData.bereinigungEbit.map((item, index) => {
-            const kennzahlenDataEbit = parseFloat(kennzahlenDataEbits[index].value) || 0;
-            const gehaltValue = parseFloat(gehalt[index].value) || 0;
-            const anpassungEbitValue = parseFloat(anpassungEbit[index].value) || 0;
-            const typischGehaltValue = parseFloat(typischGehalt) || 0;
-            const bereinigtesEbitValue = (kennzahlenDataEbit + gehaltValue + anpassungEbitValue) - typischGehaltValue;
-            console.log("anpassungEbitValue", anpassungEbitValue);
-            console.log("bereinigtesEbitValue", bereinigtesEbitValue);
+                console.log("anpassungEbitValue", anpassungEbitValue);
+                console.log("bereinigtesEbitValue", bereinigtesEbitValue);
 
-            return {
-                ...item,
-                value: bereinigtesEbitValue,
-            };
-        });
+                return {
+                    ...item,
+                    value: bereinigtesEbitValue,
+                };
+            });
+        };
 
-        dispatch(setBereinigungEbitValue(updatedBereinigungEbit));
-
-        return updatedBereinigungEbit;
+        if (prognose) {
+            const newBereinigungEbit = [...bereinigungData.bereinigungEbit.filter(item => item.year !== 2023), prognose2023.bereinigungEbit];
+            const updateBereinigungEbit = updateBereinigungEbit(newBereinigungEbit);
+            dispatch(setBereinigungEbitValue(updateBereinigungEbit));
+        } else {
+            const initialBereinigungEbit = bereinigungData.bereinigungEbit.filter(item => item.year !== 2023);
+            const updateBereinigungEbit = updateBereinigungEbit(initialBereinigungEbit);
+            dispatch(setBereinigungEbitValue(updateBereinigungEbit));
+        }
     };
 
     const [popoverData, setPopoverData] = useState({
