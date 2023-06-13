e4342840ec2fe5eabcd4fdaedf171baa580d7326
viet <37694614+quocvietphung@users.noreply.github.com>
2023-06-13 17:36:14 +0200

    Refactor

--- src/components/Unternehmenswertrechner/Anlass.js
+++ src/components/Unternehmenswertrechner/Anlass.js
@@ -1,69 +1,82 @@
-import React from 'react';
-import {Grid, Header, Divider, Form, Radio, Button} from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Grid, Header, Divider, Form, Radio, Button, Icon } from 'semantic-ui-react';
 
 const Anlass = (props) => {
     const radioOptions = [
-        "Unternehmensverkauf",
-        "Unternehmenskauf",
-        "Nachfolgeplanung",
-        "Aus Interesse / Akademische Nutzung"
+        {
+            value: "Unternehmensverkauf",
+            icon: "dollar sign",
+            label: "Unternehmensverkauf"
+        },
+        {
+            value: "Unternehmenskauf",
+            icon: "shopping cart",
+            label: "Unternehmenskauf"
+        },
+        {
+            value: "Nachfolgeplanung",
+            icon: "users",
+            label: "Nachfolgeplanung"
+        },
+        {
+            value: "Aus Interesse / Akademische Nutzung",
+            icon: "graduation cap",
+            label: "Aus Interesse / Akademische Nutzung"
+        }
     ];
 
-    const hiddenInputs = {
-        anlass: "",
-        submissionDate: "2023-05-24",
-        ertragswertHiddenFullInput: "22.743.294",
-        ertragswertHiddenFormattedInput: "22.7 Mio",
-        submissionKey: "33cfe8bd63690b5aeb560f364f1d6ef03fa5b82f6447e3f13d3a274fadfd238f"
-    };
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleRadioChange = (e, { value }) => setSelectedOption(value);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // You can work with selectedOption here
+        console.log(selectedOption);
+    }
 
     return (
         <Grid padded className="shared-section anlass">
             <Grid.Column>
                 <Header as="h2">6. Anlass für den Unternehmenswertrechner</Header>
                 <Divider />
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Header as="h3">
                         Bitte wählen Sie den Anlass der Bewertung aus:
                         <span className="required-mark">*</span>
                     </Header>
                     <Form.Field className="anlass-wrapper">
-                        {radioOptions.map((option) => (
-                            <Form.Field key={option}>
-                                <Radio
-                                    label={option}
-                                    name="anlass"
-                                    value={option}
-                                    required
-                                />
-                            </Form.Field>
-                        ))}
-                    </Form.Field>
-                    {Object.entries(hiddenInputs).map(([name, value]) => (
-                        <input
-                            type="hidden"
-                            name={name}
-                            value={value}
-                            className="d-none"
-                            key={name}
-                        />
-                    ))}
-
-                    <Form.Field>
+                        <Grid columns={2} divided>
+                            <Grid.Row>
+                                {radioOptions.map((option) => (
+                                    <Grid.Column key={option.value}>
+                                        <Button
+                                            fluid
+                                            icon
+                                            labelPosition="left"
+                                            className={selectedOption === option.value ? 'selected-option' : ''}
+                                            onClick={() => handleRadioChange(null, { value: option.value })}
+                                        >
+                                            <Icon name={option.icon} />
+                                            {option.label}
+                                        </Button>
+                                    </Grid.Column>
+                                ))}
+                            </Grid.Row>
+                        </Grid>
                         <p className="required-fields-hint">
                             <span className="required">*</span>Diese Eingaben sind Pflichtfelder
                         </p>
-                    </Form.Field>
 
-                    <Form.Field>
                         <div className="button-container">
-                            <Button className="click-back" onClick={props.onZuruckClick}>Zurück</Button>
+                            <Button className="click-back" onClick={props.onZuruckClick}>
+                                Zurück
+                            </Button>
                             <Button primary type="submit">
                                 Bewertung abschließen
                             </Button>
                         </div>
                     </Form.Field>
-
                 </Form>
             </Grid.Column>
         </Grid>
