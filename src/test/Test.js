9a1e48fa8e978f76da3d14fb41b1e4417d7c223f
viet <37694614+quocvietphung@users.noreply.github.com>
2023-06-14 17:23:49 +0200

    Refactor

--- src/test/Test.js
+++ src/test/Test.js
@@ -1,57 +1,71 @@
 import React from 'react';
-import { Grid, Header, Divider, Form, Radio } from 'semantic-ui-react';
+import { Grid, Header, Divider, Form, Container, Segment, Input, Button, Checkbox } from 'semantic-ui-react';
 import './Test.scss';
 
 const Test = () => {
-    const radioOptions = [
-        "Unternehmensverkauf",
-        "Unternehmenskauf",
-        "Nachfolgeplanung",
-        "Aus Interesse / Akademische Nutzung"
-    ];
-
-    const hiddenInputs = {
-        anlass: "",
-        submissionDate: "2023-05-24",
-        ertragswertHiddenFullInput: "22.743.294",
-        ertragswertHiddenFormattedInput: "22.7 Mio",
-        submissionKey: "33cfe8bd63690b5aeb560f364f1d6ef03fa5b82f6447e3f13d3a274fadfd238f"
-    };
-
     return (
-        <Grid padded className="Test">
-            <Grid.Column>
-                <Header as="h2">6. Anlass für den Unternehmenswertrechner</Header>
-                <Divider />
-                <Form>
-                    <Header as="h3">
-                        Bitte wählen Sie den Anlass der Bewertung aus:
-                        <span className="required-mark">*</span>
-                    </Header>
-                    <Form.Field className="anlass-wrapper">
-                        {radioOptions.map((option) => (
-                            <Form.Field key={option}>
-                                <Radio
-                                    label={option}
-                                    name="anlass"
-                                    value={option}
-                                    required
-                                />
-                            </Form.Field>
-                        ))}
-                    </Form.Field>
-                    {Object.entries(hiddenInputs).map(([name, value]) => (
-                        <input
-                            type="hidden"
-                            name={name}
-                            value={value}
-                            className="d-none"
-                            key={name}
-                        />
-                    ))}
-                </Form>
-            </Grid.Column>
-        </Grid>
+        <Container as="section" className="container-section">
+            <Grid>
+                <Grid.Row>
+                    <Grid.Column>
+                        <Header as="h2" size="xl" className="header-xl">
+                            Der Wert Ihres Unternehmens beträgt:
+                        </Header>
+                        <Segment className="value-segment">
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column>
+                                        <p>€31.396.921</p>
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Segment>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Header as="h2" size="xl" className="pdf-header-xl">
+                            Laden Sie jetzt Ihren <br />persönlichen PDF-Bericht <br />herunter
+                        </Header>
+                        <Segment className="pdf-segment">
+                            <p>Anonym, kostenlos und unverbindlich</p>
+                        </Segment>
+                        <Divider />
+                        <Segment className="form-segment">
+                            <Grid>
+                                <Form>
+                                    <Grid.Row>
+                                        <Grid.Column>
+                                            <Form.Field>
+                                                <label className="email-label">Ihre E-Mail Adresse</label>
+                                                <Input type="email" name="email" required className="email-input" />
+                                                <Segment className="email-error-segment">
+                                                    Das ist ein Pflichtfeld
+                                                </Segment>
+                                            </Form.Field>
+                                        </Grid.Column>
+                                        <Grid.Column>
+                                            <Button type="submit" className="submit-button">Bericht anfragen</Button>
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                    <Grid.Row>
+                                        <Grid.Column>
+                                            <Form.Field>
+                                                <Checkbox id="invalidCheck" required />
+                                                <label htmlFor="invalidCheck" className="checkbox-label">
+                                                    Hiermit erkläre ich mich mit der Verarbeitung meiner Daten zur Bearbeitung meiner Anfrage gemäß der <a href="https://everto-consulting.de/datenschutz/" target="_blank" title="Zur Datenschutzerklärung">Datenschutzerklärung</a> einverstanden.
+                                                </label>
+                                                <Segment>
+                                                    Bitte akzeptieren Sie die Datenschutzbestimmungen
+                                                </Segment>
+                                            </Form.Field>
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                </Form>
+                            </Grid>
+                        </Segment>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </Container>
     );
 };
 
