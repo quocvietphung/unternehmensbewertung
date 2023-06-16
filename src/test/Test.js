a6bc9cec380bd66e7ad12b4f4fe2cfea894599a7
viet <37694614+quocvietphung@users.noreply.github.com>
2023-06-16 10:34:25 +0200

    Refactor

--- src/test/Test.js
+++ src/test/Test.js
@@ -1,71 +1,14 @@
 import React from 'react';
-import { Grid, Header, Divider, Form, Container, Segment, Input, Button, Checkbox } from 'semantic-ui-react';
-import './Test.scss';
+import { Header } from 'semantic-ui-react';
+import './Test.css'; // Import file CSS
 
 const Test = () => {
+    const unternehmenswert = 'Der Wert Ihres Unternehmens beträgt:';
+
     return (
-        <Container as="section" className="container-section">
-            <Grid>
-                <Grid.Row>
-                    <Grid.Column>
-                        <Header as="h2" size="xl" className="header-xl">
-                            Der Wert Ihres Unternehmens beträgt:
-                        </Header>
-                        <Segment className="value-segment">
-                            <Grid>
-                                <Grid.Row>
-                                    <Grid.Column>
-                                        <p>€31.396.921</p>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Segment>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Header as="h2" size="xl" className="pdf-header-xl">
-                            Laden Sie jetzt Ihren <br />persönlichen PDF-Bericht <br />herunter
-                        </Header>
-                        <Segment className="pdf-segment">
-                            <p>Anonym, kostenlos und unverbindlich</p>
-                        </Segment>
-                        <Divider />
-                        <Segment className="form-segment">
-                            <Grid>
-                                <Form>
-                                    <Grid.Row>
-                                        <Grid.Column>
-                                            <Form.Field>
-                                                <label className="email-label">Ihre E-Mail Adresse</label>
-                                                <Input type="email" name="email" required className="email-input" />
-                                                <Segment className="email-error-segment">
-                                                    Das ist ein Pflichtfeld
-                                                </Segment>
-                                            </Form.Field>
-                                        </Grid.Column>
-                                        <Grid.Column>
-                                            <Button type="submit" className="submit-button">Bericht anfragen</Button>
-                                        </Grid.Column>
-                                    </Grid.Row>
-                                    <Grid.Row>
-                                        <Grid.Column>
-                                            <Form.Field>
-                                                <Checkbox id="invalidCheck" required />
-                                                <label htmlFor="invalidCheck" className="checkbox-label">
-                                                    Hiermit erkläre ich mich mit der Verarbeitung meiner Daten zur Bearbeitung meiner Anfrage gemäß der <a href="https://everto-consulting.de/datenschutz/" target="_blank" title="Zur Datenschutzerklärung">Datenschutzerklärung</a> einverstanden.
-                                                </label>
-                                                <Segment>
-                                                    Bitte akzeptieren Sie die Datenschutzbestimmungen
-                                                </Segment>
-                                            </Form.Field>
-                                        </Grid.Column>
-                                    </Grid.Row>
-                                </Form>
-                            </Grid>
-                        </Segment>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </Container>
+        <Header as="h2" className="heading-title">
+            {unternehmenswert}
+        </Header>
     );
 };
 
