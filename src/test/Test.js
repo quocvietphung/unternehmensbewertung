d1e9596707b70c8605a143b27b4e909393a96bb0
viet <37694614+quocvietphung@users.noreply.github.com>
2023-06-16 15:56:38 +0200

    Refactor

--- src/test/Test.js
+++ src/test/Test.js
@@ -6,37 +6,35 @@ const Test = () => {
     const unternehmenswert = 'Der Wert Ihres Unternehmens beträgt:';
 
     return (
-        <Segment className="segment">
-            <Grid className="custom-grid">
-                <Grid.Row columns={2}>
-                    <Grid.Column width={6}>
-                        <div className="heading-container">
-                            <h2 className="heading-title">{unternehmenswert}</h2>
-                        </div>
-                    </Grid.Column>
-                    <Grid.Column width={10} className="custom-column">
-                        <Form className="custom-form">
-                            <Form.Field>
-                                <h3 className="form-title">
-                                    Laden Sie jetzt Ihren persönlichen PDF-Bericht herunter
-                                </h3>
-                                <p className="form-description">
-                                    Anonym, kostenlos und unverbindlich
-                                </p>
-                            </Form.Field>
-                            <Form.Input
-                                label="Ihre E-Mail Adresse"
-                                placeholder="Enter your E-Mail adresse"
-                                className="form-input"
-                            />
-                            <Button type="submit" primary className="form-button">
-                                Bericht anfragen
-                            </Button>
-                        </Form>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </Segment>
+        <Grid className="custom-grid">
+            <Grid.Row columns={2}>
+                <Grid.Column width={6}>
+                    <div className="heading-container">
+                        <h2 className="heading-title">{unternehmenswert}</h2>
+                    </div>
+                </Grid.Column>
+                <Grid.Column width={10} className="custom-column">
+                    <Form className="custom-form">
+                        <Form.Field>
+                            <h3 className="form-title">
+                                Laden Sie jetzt Ihren persönlichen PDF-Bericht herunter
+                            </h3>
+                            <p className="form-description">
+                                Anonym, kostenlos und unverbindlich
+                            </p>
+                        </Form.Field>
+                        <Form.Input
+                            label="Ihre E-Mail Adresse"
+                            placeholder="Enter your E-Mail adresse"
+                            className="form-input"
+                        />
+                        <Button type="submit" primary className="form-button">
+                            Bericht anfragen
+                        </Button>
+                    </Form>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
     );
 };
 
