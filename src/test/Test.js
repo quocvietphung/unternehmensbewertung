befea069be3cda1c15f37602b8764b94fbfe6f85
viet <37694614+quocvietphung@users.noreply.github.com>
2023-06-16 12:11:04 +0200

    Refactor

--- src/test/Test.js
+++ src/test/Test.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Grid, Form, Button } from 'semantic-ui-react';
 import './Test.scss';
 
@@ -6,22 +6,29 @@ const Test = () => {
     const unternehmenswert = 'Der Wert Ihres Unternehmens beträgt:';
 
     return (
-        <Grid>
+        <Grid className="custom-grid">
             <Grid.Row columns={2}>
                 <Grid.Column width={6}>
-                    <h2 className="heading-title">{unternehmenswert}</h2>
+                    <div className="heading-container">
+                        <h2 className="heading-title">{unternehmenswert}</h2>
+                    </div>
                 </Grid.Column>
                 <Grid.Column width={10} className="custom-column">
-                    <Form>
+                    <Form className="custom-form">
                         <Form.Field>
-                            <h3>Laden Sie jetzt Ihren persönlichen PDF-Bericht herunter</h3>
-                            <p>Anonym, kostenlos und unverbindlich</p>
+                            <h3 className="form-title">
+                                Laden Sie jetzt Ihren persönlichen PDF-Bericht herunter
+                            </h3>
+                            <p className="form-description">
+                                Anonym, kostenlos und unverbindlich
+                            </p>
                         </Form.Field>
                         <Form.Input
                             label="Ihre E-Mail Adresse"
                             placeholder="Enter your E-Mail adresse"
+                            className="form-input"
                         />
-                        <Button type="submit" primary>
+                        <Button type="submit" primary className="form-button">
                             Bericht anfragen
                         </Button>
                     </Form>
