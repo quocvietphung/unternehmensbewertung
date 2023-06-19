bfba72fedbedf2ed2b0a4164c2ea1a389ae36607
viet <37694614+quocvietphung@users.noreply.github.com>
2023-06-19 18:06:14 +0200

    Refactor

--- src/test/PDF.js
+++ src/test/PDF.js
@@ -28,37 +28,16 @@ const PDF = () => {
             <Page className="page" size="A4">
                 <View>
                     <Text className="text" style={{ fontSize: 15, margin: 20 }}>
-                        Python is a popular, powerful, flexible, and easy-to-learn programming language.
-                        It has clear and readable syntax, which helps reduce program maintenance costs.
-                        Python supports multiple programming paradigms, including object-oriented and
-                        functional programming. Python is also widely used in data analysis,
-                        data science, artificial intelligence, and machine learning.
+                        First Name: {ergebnisData.firstName}
                     </Text>
-                </View>
-                <View>
                     <Text className="text" style={{ fontSize: 15, margin: 20 }}>
-                        Some key features of Python include: Easy to learn and use, Clear syntax,
-                        Large programming community, Powerful and flexible, Supports numerous libraries
-                        and extensions, Cross-platform and Multi-paradigm programming.
+                        Last Name: {ergebnisData.lastName}
                     </Text>
-                </View>
-            </Page>
-            <Page className="page" size="A4">
-                <View>
                     <Text className="text" style={{ fontSize: 15, margin: 20 }}>
-                        For beginners, Python is an excellent language to start with.
-                        It has a low learning curve and comes with a wealth of learning materials and
-                        online resources. For experienced programmers, Python provides
-                        a powerful and flexible language that can be used to perform a variety of
-                        programming tasks.
+                        Email: {ergebnisData.email}
                     </Text>
-                </View>
-                <View>
                     <Text className="text" style={{ fontSize: 15, margin: 20 }}>
-                        Python has many applications, from web development, data analysis, machine learning
-                        to software programming and game development. Whether you are looking for a
-                        first programming language or a new language to add to your toolkit, Python is
-                        a good choice.
+                        Unternehmenswert: {unternehmenswert}
                     </Text>
                 </View>
             </Page>
