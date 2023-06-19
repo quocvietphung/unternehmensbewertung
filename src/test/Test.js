4a1eddb07e69b4ec92dca79b846c85d10a26f992
viet <37694614+quocvietphung@users.noreply.github.com>
2023-06-19 14:25:12 +0200

    Refactor

--- src/test/Test.js
+++ src/test/Test.js
@@ -6,13 +6,21 @@ import './Test.scss';
 const Test = () => {
     const unternehmenswert = useSelector((state) => state.sections.sectionData.unternehmenswert);
 
+    const formatUnternehmenswert = (unternehmenswert) => {
+        const roundedValue = Math.round(unternehmenswert).toString();
+
+        const formattedValue = roundedValue.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+        return formattedValue;
+    }
+
     return (
-        <Grid className="custom-grid">
+        <Grid className="enterprise-result">
             <Grid.Row columns={2}>
                 <Grid.Column width={6}>
                     <div className="heading-container">
                         <h2 className="heading-title">Der Wert Ihres Unternehmens beträgt:</h2>
-                        <p className="ertragswert">{unternehmenswert}</p>
+                        <p className="ertragswert">{formatUnternehmenswert(unternehmenswert)} €</p>
                     </div>
                 </Grid.Column>
                 <Grid.Column width={10} className="custom-column">
@@ -21,10 +29,18 @@ const Test = () => {
                             <h3 className="form-title">
                                 Laden Sie jetzt Ihren persönlichen PDF-Bericht herunter
                             </h3>
-                            <p className="form-description">
-                                Anonym, kostenlos und unverbindlich
-                            </p>
+                            <p className="form-description">Anonym, kostenlos und unverbindlich</p>
                         </Form.Field>
+                        <Form.Input
+                            label="Vorname"
+                            placeholder="Ihr Vorname"
+                            className="form-input"
+                        />
+                        <Form.Input
+                            label="Nachname"
+                            placeholder="Ihr Nachname"
+                            className="form-input"
+                        />
                         <Form.Input
                             label="Ihre E-Mail Adresse"
                             placeholder="Enter your E-Mail adresse"
