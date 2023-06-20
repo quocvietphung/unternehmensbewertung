95b707a141be27f4264cb6e4964e64487d9290ff
viet <37694614+quocvietphung@users.noreply.github.com>
2023-06-20 10:28:19 +0200

    Create server

---
+++ src/server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/send-email', (req, res) => {
+    const { to, subject, body } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ionos.de',
+        auth: {
+            user: 'solarrechner@sternsystems.de',
+            pass: 'VIImudPnEhAgIIQ,UggDQ+926o1ZG0ir'
+        }
+    });
+
+    const mailOptions = {
+        from: 'solarrechner@sternsystems.de',
+        to: to,
+        subject: subject,
+        text: body
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error:', error);
+            res.status(500).json({ error: 'Failed to send email' });
+        } else {
+            console.log('Email sent:', info.response);
+            res.json({ message: 'Email sent successfully' });
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
