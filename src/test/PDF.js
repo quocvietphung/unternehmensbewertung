9802704b98c8e51a6b08b8c6e81f6f87dcd5647a
viet <37694614+quocvietphung@users.noreply.github.com>
2023-06-20 16:35:09 +0200

    Create server

--- src/test/PDF.js
+++ src/test/PDF.js
@@ -44,12 +44,44 @@ const PDF = () => {
         </Document>
     );
 
+    const sendEmail = () => {
+        const data = {
+            to: 'quocvietphung1993@gmail.com',
+            subject: 'Your PDF',
+            body: 'Please find the PDF attached.',
+            attachments: [{
+                filename: 'test.pdf',
+                path: 'path/to/example.pdf' // Thay đổi đường dẫn tới tệp PDF thật của bạn
+            }]
+        };
+
+        fetch('/send-email', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(result => {
+                console.log('Email sent:', result);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    };
+
     return (
         <Grid container stackable centered className="grid-container">
             <Grid.Row>
                 <Grid.Column>
                     <h1>Create and View PDF</h1>
-                    {!isViewing && <Button className="button" onClick={handleView}>View PDF</Button>}
+                    {!isViewing && (
+                        <div>
+                            <Button className="button" onClick={handleView}>View PDF</Button>
+                            <Button className="button" onClick={sendEmail}>Send via Email</Button>
+                        </div>
+                    )}
                     {isViewing && (
                         <div>
                             <PDFViewer className="pdf-viewer" width={600} height={800}>
