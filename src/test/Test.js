2689d545d09d0e182ca25caeef812f7c33abdcbd
viet <37694614+quocvietphung@users.noreply.github.com>
2023-06-20 10:44:35 +0200

    Create server

--- src/test/Test.js
+++ src/test/Test.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Grid, Form, Button } from 'semantic-ui-react';
-import { useSelector } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import './Test.scss';
+import {setErgebnisData} from "../redux/ergebnisSlice";
 
 const Test = () => {
     const unternehmenswert = useSelector((state) => state.sections.sectionData.unternehmenswert);
+    const ergebnisData = useSelector((state) => state.ergebnis.ergebnisData);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        // checkValidity();
+        console.log("ergebnisData", ergebnisData);
+    }, [ergebnisData]);
 
     const formatUnternehmenswert = (unternehmenswert) => {
         const roundedValue = Math.round(unternehmenswert).toString();
-
         const formattedValue = roundedValue.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-
         return formattedValue;
+    }
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        dispatch(setErgebnisData({ ...ergebnisData, [name]: value }));
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = {
+            firstName: e.target.firstName.value,
+            lastName: e.target.lastName.value,
+            email: e.target.email.value,
+        };
+        dispatch(setErgebnisData(formData));
     }
 
     return (
@@ -24,7 +45,7 @@ const Test = () => {
                     </div>
                 </Grid.Column>
                 <Grid.Column width={10} className="result-column">
-                    <Form className="result-form">
+                    <Form className="result-form" onSubmit={handleSubmit}>
                         <Form.Field>
                             <h3 className="form-title">
                                 Laden Sie jetzt Ihren persönlichen PDF-Bericht herunter
@@ -35,16 +56,25 @@ const Test = () => {
                             label="Vorname"
                             placeholder="Ihr Vorname"
                             className="form-input"
+                            name="firstName"
+                            value={ergebnisData.firstName}
+                            onChange={handleInputChange}
                         />
                         <Form.Input
                             label="Nachname"
                             placeholder="Ihr Nachname"
                             className="form-input"
+                            name="lastName"
+                            value={ergebnisData.lastName}
+                            onChange={handleInputChange}
                         />
                         <Form.Input
                             label="Ihre E-Mail Adresse"
                             placeholder="Enter your E-Mail adresse"
                             className="form-input"
+                            name="email"
+                            value={ergebnisData.email}
+                            onChange={handleInputChange}
                         />
                         <Button type="submit" primary className="form-button">
                             Bericht anfragen
