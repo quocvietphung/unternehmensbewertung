dbb2dfb13d95e0172c0148ad19f8e973b17e9f7b
viet <37694614+quocvietphung@users.noreply.github.com>
2023-06-25 21:01:32 +0200

    PDF

--- src/test/MyDocument.js
+++ src/test/MyDocument.js
@@ -109,7 +109,7 @@ const MyDocument = ({ kennzahlenData, basisInfoData, bereinigungData }) => {
         },
         image: {
             margin: '0 auto',
-            width: 500,
+            width: 550,
             height: 350,
         },
         imageTitle: {
@@ -384,7 +384,7 @@ const MyDocument = ({ kennzahlenData, basisInfoData, bereinigungData }) => {
                         body: '',
                     }}
                 />
-                <Text style={styles.imageTitle}>Abbildung 1: Wertfaktor von EBIT und Umsatz (Value Factor of EBIT and Revenue)</Text>
+                <Text style={styles.imageTitle}>Abbildung 1: Der EBIT-Umsatzfaktor in verschiedenen Branchengruppen.</Text>
                 <PageNumber pageNumber="2" />
             </Page>
             <Page style={styles.page}>
@@ -421,33 +421,16 @@ const MyDocument = ({ kennzahlenData, basisInfoData, bereinigungData }) => {
 
             <Page style={styles.page}>
                 <Header />
-                <Text style={styles.content}>
-                    Der Unternehmenswert (Enterprise Value) ist ein wichtiger Kennwert bei der Finanzanalyse eines
-                    Unternehmens. Er spiegelt den Gesamtwert des Unternehmens wider, indem der Wert der Aktien und
-                    Schulden berechnet und anschließend um den vorhandenen Bargeld- und Bargeldäquivalentenbestand
-                    reduziert wird.
-                </Text>
-                <Text style={styles.content}>
-                    Um den Unternehmenswert zu berechnen, müssen Informationen über das Einkommen, den Umsatz, den
-                    Gewinn vor Steuern (EBIT) und andere relevante Faktoren gesammelt werden. Anschließend wird der
-                    Durchschnittswert des Einkommens berechnet, indem der Wert des Einkommens aggregiert und durch
-                    die Anzahl der Einträge geteilt wird. Daraufhin werden Faktoren wie Umsatz, EBIT, Branche und
-                    Position des Unternehmens verwendet, um die erforderlichen Komponenten für den Unternehmenswert
-                    zu berechnen. Das Ergebnis dieser Berechnung wird zusammengefasst, um den Unternehmenswert zu
-                    ermitteln. Der Unternehmenswert gibt den Gesamtwert des Unternehmens an und kann zur Vergleich
-                    mit dem aktuellen Marktwert des Unternehmens oder zur Bewertung des Wachstumspotenzials und der
-                    Gewinnmöglichkeiten des Unternehmens verwendet werden.
-                </Text>
                 <Image
-                    style={[styles.image, { width: 450 , height: 300}]}
+                    style={[styles.image, {width: 550 , height: 400}]}
                     source={{
-                        uri: lage,
+                        uri: marktDaten,
                         method: 'GET',
                         headers: {},
                         body: '',
                     }}
                 />
-                <Text style={styles.imageTitle}>Abbildung 2: Wertfaktor nach Lage (Value Factor based on Location)</Text>
+                <Text style={styles.imageTitle}>Abbildung 3: Wertfaktor von EBIT und Umsatz (Value Factor of EBIT and Revenue)</Text>
                 <PageNumber pageNumber="4" />
             </Page>
 
