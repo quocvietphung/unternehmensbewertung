bde1d46503d4d311178f69cf08ee8e9b9a6e6cac
viet <37694614+quocvietphung@users.noreply.github.com>
2023-06-27 11:13:23 +0200

    PDF

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,9 +10,14 @@ const Home = () => {
                     Willkommen auf unserer Webseite
                 </SemanticHeader>
                 <p className="home-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dignissim lectus id mattis dignissim.
-                    Donec commodo mi vitae nisi fermentum fringilla. Quisque sem mauris, mollis sit amet felis eu, suscipit
-                    consequat turpis. Proin elementum nibh at semper malesuada.
+
+                    Orgaplan Beratung versteht die Bedeutung des Unternehmenswerts für das Geschäft. Der Unternehmenswert (Enterprise Value) ist ein wichtiger Indikator, der den Gesamtwert eines Unternehmens repräsentiert. Er wird berechnet, indem der Marktwert aller Aktien und Anleihen sowie Schulden addiert und der Bargeld- und äquivalente Geldbeträge abgezogen werden.
+
+                    Orgaplan Beratung konzentriert sich darauf, die Leistung und Rentabilität zu verbessern, um den Unternehmenswert zu steigern. Das Unternehmen trifft strategische Entscheidungen zur Produktentwicklung, Kundenakquise und Erweiterung des Dienstleistungsportfolios, um den Umsatz und Gewinn zu steigern. Indem dies erreicht wird, kann Orgaplan Beratung den Unternehmenswert erhöhen und die Attraktivität für Investoren steigern.
+
+                    Darüber hinaus legt Orgaplan Beratung großen Wert auf den Aufbau einer starken Marktposition und einer positiven Unternehmenskultur. Durch Bereitstellung hochwertiger Dienstleistungen und den Aufbau langfristiger Kundenbeziehungen schafft das Unternehmen Vertrauen und Wettbewerbsfähigkeit auf dem Markt. Dies hat eine positive Auswirkung auf den Unternehmenswert und stärkt das Vertrauen von Investoren und Kunden.
+
+                    Auf dieser Homepage haben wir eine Methode zur Berechnung des Unternehmenswerts anhand wichtiger Kriterien wie Branche, Unternehmensalter, Umsatz, EBIT, Gewinn, Equity Bridge und Qualität entwickelt. Sie können dies tun, indem Sie die relevanten Informationen im Abschnitt "Unternehmenswert berechnen" eingeben, und das Ergebnis des Unternehmenswerts wird Ihnen als PDF-Datei per E-Mail zugesandt.
                 </p>
             </Container>
         </div>
